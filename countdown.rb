777701fd55dfe0ede322f4a81f63faa846f976d2
Jackline-ke <antoninahodhiambo@gmail.com>
2023-05-22 13:14:31 +0300

    Completed assignment

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,6 @@
-#write your code here
 
 def countdown(number)
     while number > 0
-
         puts "#{number} SECOND(S)!"
         number-=
         sleep 1
@@ -12,7 +10,6 @@ end
 
 def countdown_with_sleep(number)
     while number > 0
-
         puts "#{number} SECOND(S)!"
         number-=
         sleep 1
